c792ffd239b478b7e015b89de70b4e42893412ba
Hiram Montejano Gomez <hiram.montejano@gmail.com>
2023-06-21 17:37:21 +0200

    added run function, removed resync frunction, removed unnecesary code

--- botato.py
+++ botato.py
@@ -1,39 +1,43 @@
 import os
 from dotenv import load_dotenv
 
+import asyncio
+
 import discord
 from discord.ext import commands
 from discord import app_commands
 
+
 class Botato(commands.Bot):
-    def __init__(self, appId: int) -> None:
+    def __init__(self) -> None:
         super().__init__(
             command_prefix = "_not_designed_for_prefix_commands_", 
             intents = discord.Intents.all(),
-            application_id = appId)
+            activity = discord.Activity(type = discord.ActivityType.watching, 
+                                        name = "lo tonto que eres"))
+
+
+    def run(self) -> None:
+        load_dotenv()
+        super().run(os.getenv("TOKEN"))
+
 
     async def setup_hook(self) -> None:
         for folder in os.listdir("./cogs"):
                 await self.load_extension(f"cogs.{folder}.{folder}_cog")
                 print(f"Loaded {folder}_cog")
         try:
-            sync = await bot.tree.sync()
+            sync = await self.tree.sync()
             print(f"Synced {len(sync)} commands")
         except Exception as e:
             print(f"Failed to sync commands: {e}")
+
 
     async def on_ready(self) -> None:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="lo tonto que eres"))
         print(f'{bot.user} is connected\n')
 
-    async def resync(self) -> None:
-        try: 
-            sync = await bot.tree.sync()
-            print(f"Synced {len(sync)} commands")
-        except Exception as e:
-            print(f"Failed to sync commands: {e}")
+    
 
-if __name__ == "__main__":
-    load_dotenv()
-    bot = Botato(appId=os.getenv('APPID'))
-    bot.run(os.getenv('TOKEN'))
+if __name__ == "__main__":  
+    bot = Botato()
+    bot.run()
