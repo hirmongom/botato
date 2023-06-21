d18b1ff76330d3b7bc18221c0c406004e7103b34
Hiram Montejano Gomez <hiram.montejano@gmail.com>
2023-06-21 16:23:16 +0200

    Readded descriptions. Removed Test command. Changed behaviour of checking for admin in resync and reload commands

--- cogs/debug/debug_cog.py
+++ cogs/debug/debug_cog.py
@@ -8,48 +8,34 @@ class Debug(commands.Cog):
         self.bot = bot
 
     @app_commands.command(
-        name = "ping",)
+        name = "ping",
+        description = "Returns Pong!")
     async def ping(self, interaction: discord.Interaction):
         print(f">> |ping| from {interaction.user.name}")
-        await interaction.response.send_message("Pong")
+        await interaction.response.send_message("Pong!")
 
     @app_commands.command(
-        name = "echo")
+        name = "echo",
+        description = "Echoes a message")
     async def echo(self, interaction: discord.Interaction, message: str):
         print(f">> |echo| from {interaction.user.name} with message |{message}|")
-        await interaction.response.send_message(message)
+        await interaction.response.send_message(message)       
 
     @app_commands.command(
-        name = "test",
-        description = "Test command")
-    @app_commands.describe(number = "Choice test")
-    @app_commands.choices(number = [
-        Choice(name = "one", value = 1),
-        Choice(name = "two", value = 2),
-        Choice(name = "three", value = 3)
-    ])
-    async def test(self, interaction: discord.Interaction, number: int):
-        print(f">> |test| from {interaction.user.name} with number |{number}|")
-        await interaction.response.send_message(f"Number: {number}")
-        
-
-    @app_commands.command(
-        name = "reload")
+        name = "reload",
+        description = "Reload a specific cog")
+    @app_commands.checks.has_permissions(administrator = True)
     async def reload(self, interaction: discord.Interaction, cog: str):
         print(f">> |reload| from {interaction.user.name} with cog |{cog}|")
-        if interaction.user.name != self.bot.admin:
-            await interaction.response.send_message("No permission")
-            return
         await self.bot.reload_extension(f"cogs.{cog}.{cog}_cog")
         await interaction.response.send_message(f"Reloaded {cog}")
 
     @app_commands.command(
-        name = "resync")
+        name = "resync",
+        description = "Reloads all cogs and resyncs")
+    @app_commands.checks.has_permissions(administrator = True)
     async def resync(self, interaction: discord.Interaction):
         print(f">> |resync| from {interaction.user.name}")
-        if interaction.user.name != self.bot.admin:
-            await interaction.response.send_message("No permission")
-            return
 
         await interaction.response.defer()
 
@@ -60,6 +46,7 @@ class Debug(commands.Cog):
         await self.bot.resync()
 
         await interaction.followup.send("Resynced commands")
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Debug(bot))
