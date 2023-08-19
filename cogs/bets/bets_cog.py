0c53262529d6ca684f6b697654ed89ede0bb39ef
Hiram Montejano Gomez <hiram.montejano@gmail.com>
2023-08-19 15:30:37 +0200

    initial bet() embed

--- cogs/bets/bets_cog.py
+++ cogs/bets/bets_cog.py
@@ -13,6 +13,14 @@ class Bets(commands.Cog):
   def __init__(self, bot: commands.Bot) -> None:
     self.bot = bot
     self.week_day = 0
+    self.ready_bets = []
+
+
+  def update_data(self, sport: str) -> None:
+    if sport == "f1":
+      self.bot.logger.info("Fetching F1 data")
+      self.bot.web_scrapper.get_f1_data()
+      self.bot.logger.info("Fetched F1 data")
 
 
   def get_next_f1_event(self) -> None:
@@ -20,11 +28,11 @@ class Bets(commands.Cog):
       race_data = {}
       reader = csv.reader(file)
       for row in reader:
-        if row[3] == "":
-          print(f"Next race: {row[0]}/{row[1]} {row[2]}")
-          race_data["day"] = row[0]
-          race_data["month"] = row[1]
-          race_data["race"] = row[2]
+        if row[4] == "":
+          race_data["ix"] = row[0]
+          race_data["day"] = row[1]
+          race_data["month"] = row[2]
+          race_data["event"] = row[3]
           race_data["pool"] = 500
           race_data["status"] = "open"
           save_json(race_data, "f1/f1_bet", "bets")
@@ -44,12 +52,50 @@ class Bets(commands.Cog):
 
   async def daily_task(self) -> None:
     self.bot.interaction_logger.info("Bets daily task")
+
+    # If an event was completed yesterday process the bet
+    if len(self.ready_bets) > 0:
+      for sport in self.ready_bets:
+        self.bot.interaction_logger.info(f"Processing bet for {sport}")
+        self.update_data(sport)
+      self.ready_bets = []
+        
+
+    # If an event happens today, mark it to process it tomorrow
     now = datetime.now()
     for sport in os.listdir("data/bets/"):
       data = load_json(f"{sport}/{sport}_bet", "bets")
       if int(data["day"]) == now.day and int(data["month"]) == now.month:
-        pass
+        self.ready_bets.append("f1")
 
+
+  @app_commands.command(
+    name = "bet",
+    description = "Check ongoing bets and try your luck!"
+  )
+  async def bet(self, interaction: discord.Interaction) -> None:
+    self.bot.interaction_logger.info(f"|bet| from {interaction.user.name}")
+    await interaction.response.defer()
+
+    emoji_mapping = {
+      "f1": "🏎️"
+    }
+
+    embed = discord.Embed(
+      title = "💰 Betting House",
+      description = f"Check the next events and place bets on them!",
+      color = discord.Colour.teal()
+    )
+    for sport in os.listdir("data/bets/"):
+      data = load_json(f"{sport}/{sport}_bet", "bets")
+      emoji = emoji_mapping[sport]
+      embed.add_field(name = f"📅 {data['day']}/{data['month']} {sport.upper()}",
+                      value = f"{emoji} {data['event']} ➡️ Current pool = {data['pool']}€",
+                      inline = False)
+    embed.set_footer(text = "Lucky Betting | Botato Bets", icon_url = self.bot.user.display_avatar.url)
+
+    await interaction.followup.send(embed = embed)
+    
 
 async def setup(bot: commands.Bot) -> None:
 	await bot.add_cog(Bets(bot))
