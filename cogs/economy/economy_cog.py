760ba963bc5795d7ea9a0cfed79471fca3c76d81
Hiram Montejano Gomez <hiram.montejano@gmail.com>
2023-08-23 11:10:11 +0200

    fixed weekly trigger

--- cogs/economy/economy_cog.py
+++ cogs/economy/economy_cog.py
@@ -22,6 +22,7 @@ class Economy(commands.Cog):
 
 
   async def daily_task(self) -> None:
+    week_day = (week_day + 1) % 7
     for file in os.listdir("data/economy/"):
       if file != ".gitkeep":
         data = load_json(file[:-5], "economy")
