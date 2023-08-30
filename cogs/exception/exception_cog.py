74af28fe4b86fd8e014b2c1bcd490da387ff886b
Hiram Montejano Gomez <hiram.montejano@gmail.com>
2023-08-30 07:17:49 +0200

    added traceback to log

--- cogs/exception/exception_cog.py
+++ cogs/exception/exception_cog.py
@@ -1,3 +1,5 @@
+import traceback
+
 import discord
 from discord.ext import commands
 
@@ -19,7 +21,7 @@ class Exception(commands.Cog):
   async def handle_error(self, interaction: discord.Interaction, error: discord.app_commands.AppCommandError) -> None:
     # First interaction will break most commands which require data, since it is not created yet
     # If it coccurs, create data and notify user
-    self.bot.logger.error(error)
+    self.bot.logger.error("".join(traceback.format_exception(type(error), error, error.__traceback__)))
     if isinstance(error.original, KeyError):
       user_ids = load_json("user_ids", "other")
       if not interaction.user.name in user_ids.keys():
