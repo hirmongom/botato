730a290acbb39696cf184eadd79698e453c98e7e
Hiram Montejano Gomez <hiram.montejano@gmail.com>
2023-09-09 11:21:10 +0200

    fixed error

--- utils/achievement.py
+++ utils/achievement.py
@@ -21,7 +21,8 @@ lock = asyncio.Lock()
 
 
 #***************************************************************************************************
-async def update_achievment(interaction: discord.Interaction, achievement_data: dict, stat: str, value: int, tier: int) -> None:
+async def update_achievment(interaction: discord.Interaction, achievement_data: dict, 
+                            user_data: dict, stat: str, value: int, tier: int) -> None:
   map_stat_name = {
     "days_interacted": "Days Engaged",
     "command_count": "Commands Executed",
@@ -46,139 +47,140 @@ async def update_achievment(interaction: discord.Interaction, achievement_data: 
         break
 
   xp_increase = tier * 100
-  achievement_data["experience"] += xp_increase
+  user_data["experience"] += xp_increase
 
   await interaction.followup.send(f"You unlocked the achievment {value} {map_stat_name[stat]} "
                                   f"and received {xp_increase} XP!")
-  if achievement_data["experience"] >= (achievement_data["level"] * 100 + (achievement_data["level"] - 1) * 50):
-    achievement_data["level"] += 1
-    await interaction.followup.send(f"(*) You leveled up to level {achievement_data['level']}!!")
+  if user_data["experience"] >= (user_data["level"] * 100 + (user_data["level"] - 1) * 50):
+    user_data["level"] += 1
+    await interaction.followup.send(f"(*) You leveled up to level {user_data['level']}!!")
 
 
 #***************************************************************************************************
 async def add_user_stat(stat: str, interaction: discord.Interaction) -> None:
   async with lock:
     achievement_data = load_json(interaction.user.name, "achievement")
+    user_data = load_json(interaction.user.name, "user")
 
     if stat == "days_interacted":
       achievement_data[stat] += 1
       if achievement_data[stat] == 10:
-        await update_achievment(interaction, achievement_data, stat, 10, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 10, 1)
       if achievement_data[stat] == 25:
-        await update_achievment(interaction, achievement_data, stat, 25, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 25, 2)
       if achievement_data[stat] == 50:
-        await update_achievment(interaction, achievement_data, stat, 50, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 50, 3)
       if achievement_data[stat] == 100:
-        await update_achievment(interaction, achievement_data, stat, 100, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 100, 4)
 
     if stat == "command_count":
       achievement_data[stat] += 1
       if achievement_data[stat] == 100:
-        await update_achievment(interaction, achievement_data, stat, 100, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 100, 1)
       if achievement_data[stat] == 250:
-        await update_achievment(interaction, achievement_data, stat, 250, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 250, 2)
       if achievement_data[stat] == 500:
-        await update_achievment(interaction, achievement_data, stat, 500, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 500, 3)
       if achievement_data[stat] == 1000:
-        await update_achievment(interaction, achievement_data, stat, 1000, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 1000, 4)
 
     elif stat == "gamekeys_searched":
       achievement_data[stat] += 1
       if achievement_data[stat] == 10:
-        await update_achievment(interaction, achievement_data, stat, 10, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 10, 1)
       if achievement_data[stat] == 25:
-        await update_achievment(interaction, achievement_data, stat, 25, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 25, 2)
       if achievement_data[stat] == 50:
-        await update_achievment(interaction, achievement_data, stat, 50, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 50, 3)
       if achievement_data[stat] == 100:
-        await update_achievment(interaction, achievement_data, stat, 100, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 100, 4)
     
     elif stat == "blackjack_hands_played":
       achievement_data[stat] += 1
       if achievement_data[stat] == 50:
-        await update_achievment(interaction, achievement_data, stat, 50, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 50, 1)
       if achievement_data[stat] == 100:
-        await update_achievment(interaction, achievement_data, stat, 100, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 100, 2)
       if achievement_data[stat] == 250:
-        await update_achievment(interaction, achievement_data, stat, 250, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 250, 3)
       if achievement_data[stat] == 500:
-        await update_achievment(interaction, achievement_data, stat, 500, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 500, 4)
 
     elif stat == "blackjack_hands_won":
       achievement_data[stat] += 1
       if achievement_data[stat] == 50:
-        await update_achievment(interaction, achievement_data, stat, 50, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 50, 1)
       if achievement_data[stat] == 100:
-        await update_achievment(interaction, achievement_data, stat, 100, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 100, 2)
       if achievement_data[stat] == 250:
-        await update_achievment(interaction, achievement_data, stat, 250, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 250, 3)
       if achievement_data[stat] == 500:
-        await update_achievment(interaction, achievement_data, stat, 500, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 500, 4)
 
     elif stat == "roulettes_played":
       achievement_data[stat] += 1
       if achievement_data[stat] == 50:
-        await update_achievment(interaction, achievement_data, stat, 50, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 50, 1)
       if achievement_data[stat] == 100:
-        await update_achievment(interaction, achievement_data, stat, 100, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 100, 2)
       if achievement_data[stat] == 250:
-        await update_achievment(interaction, achievement_data, stat, 250, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 250, 3)
       if achievement_data[stat] == 500:
-        await update_achievment(interaction, achievement_data, stat, 500, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 500, 4)
 
     elif stat == "roulettes_won":
       achievement_data[stat] += 1
       if achievement_data[stat] == 50:
-        await update_achievment(interaction, achievement_data, stat, 50, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 50, 1)
       if achievement_data[stat] == 100:
-        await update_achievment(interaction, achievement_data, stat, 100, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 100, 2)
       if achievement_data[stat] == 250:
-        await update_achievment(interaction, achievement_data, stat, 250, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 250, 3)
       if achievement_data[stat] == 500:
-        await update_achievment(interaction, achievement_data, stat, 500, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 500, 4)
 
     elif stat == "horse_races_played":
       achievement_data[stat] += 1
       if achievement_data[stat] == 50:
-        await update_achievment(interaction, achievement_data, stat, 50, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 50, 1)
       if achievement_data[stat] == 100:
-        await update_achievment(interaction, achievement_data, stat, 100, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 100, 2)
       if achievement_data[stat] == 250:
-        await update_achievment(interaction, achievement_data, stat, 250, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 250, 3)
       if achievement_data[stat] == 500:
-        await update_achievment(interaction, achievement_data, stat, 500, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 500, 4)
 
     elif stat == "horse_races_won":
       achievement_data[stat] += 1
       if achievement_data[stat] == 50:
-        await update_achievment(interaction, achievement_data, stat, 50, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 50, 1)
       if achievement_data[stat] == 100:
-        await update_achievment(interaction, achievement_data, stat, 100, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 100, 2)
       if achievement_data[stat] == 250:
-        await update_achievment(interaction, achievement_data, stat, 250, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 250, 3)
       if achievement_data[stat] == 500:
-        await update_achievment(interaction, achievement_data, stat, 500, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 500, 4)
 
     elif stat == "bets_placed":
       achievement_data[stat] += 1
       if achievement_data[stat] == 5:
-        await update_achievment(interaction, achievement_data, stat, 5, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 5, 1)
       if achievement_data[stat] == 15:
-        await update_achievment(interaction, achievement_data, stat, 15, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 15, 2)
       if achievement_data[stat] == 25:
-        await update_achievment(interaction, achievement_data, stat, 25, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 25, 3)
       if achievement_data[stat] == 50:
-        await update_achievment(interaction, achievement_data, stat, 50, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 50, 4)
 
     elif stat == "completed_daily_problems":
       achievement_data[stat] += 1
       if achievement_data[stat] == 5:
-        await update_achievment(interaction, achievement_data, stat, 5, 1)
+        await update_achievment(interaction, achievement_data, user_data, stat, 5, 1)
       if achievement_data[stat] == 15:
-        await update_achievment(interaction, achievement_data, stat, 15, 2)
+        await update_achievment(interaction, achievement_data, user_data, stat, 15, 2)
       if achievement_data[stat] == 25:
-        await update_achievment(interaction, achievement_data, stat, 25, 3)
+        await update_achievment(interaction, achievement_data, user_data, stat, 25, 3)
       if achievement_data[stat] == 50:
-        await update_achievment(interaction, achievement_data, stat, 50, 4)
+        await update_achievment(interaction, achievement_data, user_data, stat, 50, 4)
 
     save_json(achievement_data, interaction.user.name, "achievement")
